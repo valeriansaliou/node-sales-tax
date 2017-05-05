386f3027a4072edceb039dd0806a78c17df55901
Valerian Saliou <valerian@valeriansaliou.name>
2017-05-05 10:23:06 +0200

    Initialize
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

---
+++ test/sales_tax-test.js
@@ -0,0 +1,18 @@
+/*
+ * node-sales-tax
+ *
+ * Copyright 2017, Valerian Saliou
+ * Author: Valerian Saliou <valerian@valeriansaliou.name>
+ */
+
+
+"use strict";
+
+
+var salestax = require("../").salestax;
+var assert = require("assert");
+
+
+describe("node-fast-ratelimit", function() {
+  // TODO
+});
